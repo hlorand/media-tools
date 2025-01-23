cbd9078587729326fb5b200a3576bcf02cb20a72
Lorand Horvath <hlorand@gmail.com>
2025-01-23 05:09:08 +0100

    update compress-pdf complete rewerite

--- compress-pdf/compress-pdf.sh
+++ compress-pdf/compress-pdf.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
 
-if ! which gs &> /dev/null
-then
-  echo "Install ghostscript to continue."
-  exit
-fi
+# Compresses PDF using ImageMagick. Makes the PDF smaller in resolution and uses a grayscale palette.
+# Usage: script.sh input.pdf
+# Settings below:
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <input.pdf>"
+PAGERESOLUTION=1000 # pixels
+NUMCOLORS=16        # must be <= bitdepth^2
+BITDEPTH=4          # must be power of 2: 2,4,8...
+COLORS="sRGB"       # "Gray" (better for texts) or "sRGB"
+
+# Validate input
+if [ -z "$1" ] || [ ! -f "$1" ]; then
+    echo "Usage: $0 input.pdf"
     exit 1
 fi
 
-# /screen=72dpi, /ebook=150, /prepress=300
-gs -sDEVICE=pdfwrite \
-   -dCompatibilityLevel=1.4 \
-   -dNOPAUSE -dQUIET -dBATCH \
-   -sOutputFile=$1.compressed.pdf \
-   -dPDFSETTINGS=/ebook \
-   $1
+# Install ImageMagick if not present
+if ! command -v magick &> /dev/null; then
+    echo "ImageMagick not installed. Install: brew install imagemagick || apt install imagemagick"
+    exit 1
+fi
 
+mkdir -p jpg
+cd jpg
 
+# Convert input file to JPG
+echo "Converting to JPGs"
+magick "../$1" -density 300 -quality 90 output-%04d.jpg &
+
+pid=$! # Get the PID of the last background process
+while true; do
+  if ps -p $pid > /dev/null 2>&1; then
+    cpu=$(ps -p $pid -o %cpu=)
+    mem=$(ps -p $pid -o %mem=)
+    printf "\rCPU: %s%%, Memory: %s%%" "$cpu" "$mem"
+  else
+    break
+  fi
+  sleep 1
+done
+
+# Create GIF directory
+mkdir -p gif
+
+# Convert JPG to GIF
+echo -e "\nConverting to GIFs"
+for f in *.jpg; do 
+    magick "$f" -resize x$PAGERESOLUTION -colorspace $COLORS -colors $NUMCOLORS +dither -depth $BITDEPTH -strip "gif/${f%.jpg}.gif"
+    echo "$f"
+done
+
+cd gif
+
+# Combine GIFs into a PDF
+echo -e "\nCombining GIFs to PDF"
+magick *.gif "../../${1}-${PAGERESOLUTION}px-${NUMCOLORS}colors-${COLORS}-${BITDEPTH}bits-compressed.pdf"
+
+# Cleanup
+cd ../..
+rm -rf jpg
